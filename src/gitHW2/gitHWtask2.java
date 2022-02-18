431d8a4570c5002496f90863815db02ed37b5286
DANA CUZMICIOV <dudukuzmiciov@gmail.com>
2022-02-18 15:56:44 -0500

    commit

---
+++ src/gitHW2/gitHWtask2.java
@@ -0,0 +1,11 @@
+package gitHW2;
+
+public class gitHWtask2 {
+
+	public static void main(String[] args) {
+		 System.out.println("Dana");
+		 System.out.println("New York");
+		 System.out.println("Dana lives in New York");
+	}
+
+}
